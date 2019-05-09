66bf3755e75acfcc8b0e972488a9c850d5a0a372
c-kirkeby <Christian_Kirkeby@hotmail.com>
2019-05-09 17:18:02 +1000

    Finished complete exercise Rust: Clock

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -7,32 +7,36 @@ pub struct Clock {
 }
 
 impl Clock {
+    const DAY_IN_MINS: i32 = 1440;
+    const HOUR_IN_MINS: i32 = 60;
     pub fn new(hours: i32, minutes: i32) -> Self {
-        let min_quotient = minutes / 60;
-        let min_remainder = minutes % 60;
+        // Convert everything to minutes to simplify our calculations
+        let mut total_mins = (hours * Clock::HOUR_IN_MINS + minutes) % Clock::DAY_IN_MINS;
+
+        // If the total number of mins ends up negative, we need to compensate
+        // to make it positive.
+        if total_mins < 0 {
+            total_mins += Clock::DAY_IN_MINS;
+        }
+
+        // We get the remainder by diving the number of minutes in a day and
+        // divide that remainder by 60 to get the number of hours. Then we
+        // get the remainder of division by 60 to get the number of minutes.
         Clock {
-            hours: (hours + min_quotient) % 24,
-            minutes: min_remainder,
+            hours: total_mins % Clock::DAY_IN_MINS / Clock::HOUR_IN_MINS,
+            minutes: total_mins % Clock::HOUR_IN_MINS,
         }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        let min_quotient = self.minutes + minutes / 60;
-        let min_remainder = self.minutes + minutes % 60;
-        Clock {
-            hours: (self.hours + min_quotient) % 24,
-            minutes: min_remainder,
-        }
+        // Since the new method already deals with overflow, we can leverage
+        // it directly to get the new Clock instance.
+        Clock::new(self.hours, self.minutes + minutes)
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match (self.hours > 9, self.minutes > 9) {
-            (true, true) => write!(f, "{}:{}", self.hours, self.minutes),
-            (true, false) => write!(f, "{}:0{}", self.hours, self.minutes),
-            (false, true) => write!(f, "0{}:{}", self.hours, self.minutes),
-            (false, false) => write!(f, "0{}:0{}", self.hours, self.minutes),
-        }
+        write!(f, "{:02}:{:02}", self.hours, self.minutes)
     }
 }
